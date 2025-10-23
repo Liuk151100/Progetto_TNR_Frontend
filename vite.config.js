9c9459d9b76c525e185f33d8c8b6bb2864b5d511
Luca Faini <lucafaini20@gmail.com>
2025-10-23 21:23:22 +0200

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,32 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000000,
+    chunkSizeWarningLimit: 1000000, // 1MB
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // ogni libreria avrà il suo chunk
-            return id
-              .toString()
-              .split('node_modules/')[1]
-              .split('/')[0]
-              .toString()
-          }
+        manualChunks: {
+          // React core
+          react: ['react', 'react-dom'],
+
+          // Routing
+          router: ['react-router-dom'],
+
+          // UI framework
+          bootstrap: ['bootstrap', 'react-bootstrap', 'bootstrap-icons', 'react-bootstrap-icons'],
+
+          // Calendari pesanti
+          calendar: [
+            'react-big-calendar',
+            '@fullcalendar/react',
+            '@fullcalendar/daygrid'
+          ],
+
+          // Utility
+          utils: ['axios', 'date-fns', 'react-modal', 'react-spinners'],
         },
       },
     },
