5d0311bfcc3447213e62044da14b52557b2deadb
Luca Faini <lucafaini20@gmail.com>
2025-10-23 16:13:41 +0200

    modifiche per refresh pagina home quando viene premuta la rotella e per funzione per andare alla sezione del team

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
@@ -33,6 +33,7 @@ export default function App() {
   //   setUserOverride(true);
   //   setTheme(t => (t === 'dark' ? 'light' : 'dark'));
   // };
+
 
   return (
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,37 @@ import KartSection from "../components/KartSection";
 import LegendsSection from "../components/LegendsSection";
 import TeamSection from "../components/TeamSection";
 import ContactUs from "../components/ContactUs";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Home() {
-  
+
+
+  const location = useLocation();
+  const navigate = useNavigate()
+
+  if (location.state?.scrollTo === "team") {
+    setTimeout(() => {
+      window.scrollTo({ top: 3150, behavior: "smooth" });
+    }, 500);
+    navigate("/")
+
+  }
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [])
 
   return (
     <>
-    <KartSection/>
-    <LegendsSection/>
-    <HillclimbSection/>
-    <TeamSection/>
-    <ContactUs/>
+      <KartSection />
+      <LegendsSection />
+      <HillclimbSection />
+      <TeamSection />
+      <ContactUs />
     </>
   );
 }
