d74fd8f76d0e053cb4c5a39bfaa4ac68496ad379
Luca Faini <lucafaini20@gmail.com>
2025-10-23 21:08:42 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 10000, // ad esempio, 2 MB
+    chunkSizeWarningLimit: 1000000, // ad esempio, 2000 = 2 MB
   },
 })
